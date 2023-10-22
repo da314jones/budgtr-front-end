0ba2d678b7b26028384a2b0f236ddd95e2a26155
djones <dwaynejones@pursuit.org>
2023-10-22 19:09:08 -0400

    functionality

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import TransactionRow from './TransactionRow';
-import './SearchResults.css'
+import './TransactionList.css'; 
 
-export default function SearchResults({ transactions, index }) {
+export default function SearchResults({ transactions }) {
   const location = useLocation();
   const search = new URLSearchParams(location.search).get('search');
-
-console.log('Search parameter:', search);
-  console.log('Transactions:', transactions); 
-
   const [filteredTransactions, setFilteredTransactions] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
 
   useEffect(() => {
     if (search) {
-        console.log(transactions)
       const filtered = transactions.filter((transaction) =>
         transaction.type.toLowerCase().includes(search.toLowerCase()) ||
         transaction.date.toLowerCase().includes(search.toLowerCase()) ||
@@ -26,38 +23,56 @@ console.log('Search parameter:', search);
     }
   }, [search, transactions]);
 
+  const totalPages = Math.ceil(filteredTransactions.length / itemsPerPage);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredTransactions.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="search-results">
-      <h2>Search Results for "{search}"</h2>
-      {filteredTransactions.length === 0 ? (
+    <div className="transactions-container">
+      <div className="header">
+        <h2>Search Results for "{search}"</h2>
+      </div>
+      {currentItems.length === 0 ? (
         <p>No results found.</p>
       ) : (
         <section>
-        <table>
-          <thead>
-            <tr>
-              <th>Category</th>
-              <th>Type</th>
-              <th>Description</th>
-              <th>Amount</th>
-              <th>Date</th>
-              <th>Transaction id</th>
-              <th>Details</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredTransactions.map((transaction, index) => (
-              <TransactionRow key={transaction.id || index} transaction={transaction} />
-            ))}
-          </tbody>
-        </table>
-      </section>
-        
+          <table>
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Type</th>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Date</th>
+                <th>Transaction id</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentItems.map((transaction, index) => (
+                <TransactionRow key={transaction.id || index} transaction={transaction} />
+              ))}
+            </tbody>
+          </table>
+          <div className='table-buttons'>
+            <button onClick={() => paginate(1)} disabled={currentPage === 1}>
+              First
+            </button>
+            <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+              Previous
+            </button>
+            <button onClick={() => paginate(currentPage + 1)} disabled={currentPage === totalPages}>
+              Next
+            </button>
+            <button onClick={() => paginate(totalPages)} disabled={currentPage === totalPages}>
+              Last
+            </button>
+          </div>
+        </section>
       )}
     </div>
   );
 }
-
-
-
-
