074e70ceaf64b75a2b009ff672be57193816fa8d
djones <dwaynejones@pursuit.org>
2023-10-25 20:35:53 -0400

    removed bootstrap and associated dependecies

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-if (
-  localStorage.theme === 'dark' ||
-  (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-  document.documentElement.classList.add('dark');
-} else {
-  document.documentElement.classList.remove('dark');
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
